379de79bc2bdb5118066be0e8564ac27b95d598f
Paul O'Day <paul@pauloday.com>
2021-07-30 19:29:06 -0700

    add image output in streamlit

--- runner.py
+++ runner.py
@@ -278,6 +278,7 @@ def run_prompt(args, st, dev=0, image_name=None,):
         losses_str = ', '.join(f'{loss.item():g}' for loss in losses)
         print(f'i: {i}, loss: {sum(losses).item():g}, losses: {losses_str}')
         out = synth(z)
+        st.image(file)
         TF.to_pil_image(out[0].cpu()).save(file)
         print(f'Wrote {file}')
         
