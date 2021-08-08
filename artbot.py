230908e0bd2895210f239535e2d21339a5accde3
Paul O'Day <paul@pauloday.com>
2021-08-07 21:31:27 -0700

    fix yaml write

--- artbot.py
+++ artbot.py
@@ -9,9 +9,9 @@ def dev_count():
     return torch.cuda.device_count()
 
 # takes an input yaml file and runs it then returns a zip of the results
-def artbot(infile):
-    title, runs = parse_yaml(open(infile).read())
+def artbot(instr):
+    title, runs = parse_yaml(instr)
     batch = BatchRunner(title, runs, run_args)
     gallery = batch.run()
-    shutil.copyfile(infile, f'{gallery}/{basename(infile)}')
+    open(f'{gallery}/{title}.yml', 'w').write(str(instr))
     return shutil.make_archive(title, format='zip', root_dir=gallery)
